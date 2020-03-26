759c65f0ab7427917d81b4a6f08e9ec57413888d
Michael Skeen <mmskeen@gmail.com>
2020-03-25 20:28:13 -0600

    substituted arrow functions in

--- src/index.js
+++ src/index.js
@@ -4,29 +4,21 @@ import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// var numbers = [3, 56, 2, 48, 5];
+var numbers = [3, 56, 2, 48, 5];
 
 ////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+const newNumbers = numbers.map(x => 2 * x);
 
 //////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+const newNumbers2 = numbers.filter(x => x > 10);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+var newNumber = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
 
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+const newNumber2 = numbers.find(x => x > 10);
 
 ////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+const newNumber3 = numbers.findIndex(x => x > 10);
